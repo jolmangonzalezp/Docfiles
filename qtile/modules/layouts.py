5b70024252586a752effccf603c05ebc65d8529d
Jolman Gonzalez <jolmangonzalez5@gmail.com>
2025-03-01 19:42:04 -0500

    organized the principal directories with their files

---
+++ qtile/modules/layouts.py
@@ -0,0 +1,9 @@
+from libqtile import layout
+from libqtile.config import Match
+
+layouts = [
+    # layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
+    layout.RatioTile(),
+    layout.Max(),
+    layout.Matrix(),
+]
